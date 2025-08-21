b7fa243b7e71d2f92a6189acc1d84d2cfe8bb8aa
douglas714 <douglasnoticias@gmail.com>
2025-08-21 20:14:30 -0300

    Update LoginPage.jsx

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -1,4 +1,4 @@
-// VERSÃO MELHORADA - LoginPage_improved.jsx
+// VERSÃO MELHORADA - LoginPage.jsx
 // MELHORIAS: Design responsivo otimizado para mobile, UX aprimorada, animações suaves
 
 import { useState } from 'react';
@@ -44,7 +44,13 @@ const LoginPage = ({ onLogin }) => {
     <div className="min-h-screen bg-gradient-to-br from-blue-900 via-purple-900 to-indigo-900 relative overflow-hidden">
       {/* Background Pattern */}
       <div className="absolute inset-0 opacity-10">
-        <div className="absolute top-0 left-0 w-full h-full bg-[url('data:image/svg+xml,%3Csvg width="60" height="60" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg"%3E%3Cg fill="none" fill-rule="evenodd"%3E%3Cg fill="%23ffffff" fill-opacity="0.1"%3E%3Ccircle cx="30" cy="30" r="2"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E' )]" style={{ backgroundSize: '60px 60px' }}></div>
+        <div 
+          className="absolute top-0 left-0 w-full h-full"
+          style={{
+            backgroundImage: `url('data:image/svg+xml,%3Csvg width="60" height="60" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg"%3E%3Cg fill="none" fill-rule="evenodd"%3E%3Cg fill="%23ffffff" fill-opacity="0.1"%3E%3Ccircle cx="30" cy="30" r="2"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E' )`,
+            backgroundSize: '60px 60px'
+          }}
+        ></div>
       </div>
 
       {/* Main Content */}
